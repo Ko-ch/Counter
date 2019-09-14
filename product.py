3883d2543b7eb5f22b5151ee0cb2fb9c33cd6c99
Koichiro <koichiro_takagi@outlook.jp>
2019-09-15 00:05:21 +0900

    インスタンスなどなど 意味はさておき、何かクラス構文を覚えたぞ

--- product.py
+++ product.py
@@ -1,22 +1,27 @@
 # productクラスのインスタンス化
 class Product:
-    def __init__(self, name, price):
+    def __init__(self, name, amount):
         self.name = name  # nameというインスタンス変数
-        self.price = price
+        self.amount = amount
 
+    def discount(self, price):
+        self.amount -= price
 
 if __name__ == "__main__":
     p1 = Product("iPhone", 100000)  # Productクラスのインスタント化
     print(p1)
     print(p1.name)  # iPhone
-    print(p1.price)  # 100,000
+    p1.discount(5000)
+    print(p1.amount)  # 100,000
 
     p2 = Product("MacBookAir", 150000)
     print(p2)
     print(p2.name)
-    print(p2.price)
+    p2.discount(10000)
+    print(p2.amount)
 
     p3 = Product("AppleWatch", 50000)
     print(p3)
     print(p3.name)
-    print(p3.price)
+    p3.discount(2500)
+    print(p3.amount)
